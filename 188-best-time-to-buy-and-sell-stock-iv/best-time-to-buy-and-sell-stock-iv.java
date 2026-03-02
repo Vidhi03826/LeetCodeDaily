1b17137ebc910a91be10d9259e5e892162a51c57
Vidhi03826 <vidh312@gmail.com>
2026-03-02 22:55:12 +0530

    Time: 9 ms (20.32%) | Memory: 45.2 MB (54.88%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,33 +1,30 @@
-
-
 class Solution {
-    public int getans(int[] prices, int ind, int buy, int n,int k, int [][][] dp){
-          if (ind == n || k == 0)
-            return 0;
+    public int getans (int[] prices, int idx,int buy, int n, int k, int [][][] dp){
+        if(idx==n || k==0) return 0;
+         int profit ;
 
-        if(dp[ind][buy][k]!=-1) return dp[ind][buy][k];
-        int profit;
+          if(dp[idx][buy][k] !=-1) return dp[idx][buy][k];
 
-        if(buy==0){//can buy
-          profit = Math.max((-prices[ind] + getans(prices, ind+1,1 ,n, k, dp)) , getans(prices, ind+1,0 ,n,k,dp));
-        }
-        else {//cant buy
-            profit=  Math.max((prices[ind] + getans(prices, ind+1,0 ,n,k-1,dp)) , getans(prices, ind+1,1 ,n,k,dp));
-        }
-        dp[ind][buy][k] = profit;
-        return profit;
+         if(buy==0){
+            profit = Math.max((-prices[idx] +  getans (prices, idx+1, 1, n,k, dp)),
+                                   0         +   getans (prices, idx+1, 0, n,k, dp));
+         }
+         else{
+             profit = Math.max((prices[idx] +  getans (prices, idx+1, 0, n,k-1, dp)),
+                                   0         +   getans (prices, idx+1, 1, n,k, dp));
+         }
+         dp[idx][buy][k] = profit;
+         return profit;
+        
     }
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        
         int [][][] dp = new int [n][2][k+1];
-     for(int i=0;i<n;i++){
-        for(int j=0;j<2;j++){
-            Arrays.fill(dp[i][j] , -1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(dp[i][j], -1);
+            }
         }
-     }
-      return getans(prices, 0,0 ,n,k,dp);
-
-         
+        return getans (prices, 0, 0, n,k, dp);
     }
 }
