370e9e7bcdae310335fcc85eb82bf31d7f5a5f4d
Vidhi03826 <vidh312@gmail.com>
2026-03-13 23:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (12.84%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int indx = -1;
+
+        // Step 1: find first decreasing element from right
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                indx = i;
+                break;
+            }
+        }
+
+        // Step 2: if found, swap with just greater element
+        if (indx != -1) {
+            for (int i = n - 1; i > indx; i--) {
+                if (nums[i] > nums[indx]) {
+                    int temp = nums[i];
+                    nums[i] = nums[indx];
+                    nums[indx] = temp;
+                    break;
+                }
+            }
+        }
+
+        // Step 3: reverse the suffix
+        reverse(nums, indx + 1, n - 1);
+    }
+
+    void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
