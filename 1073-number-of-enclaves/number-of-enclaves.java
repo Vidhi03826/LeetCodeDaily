e1c56b89835efda154b62453b53ab5b2df25167f
Vidhi03826 <vidh312@gmail.com>
2026-04-06 23:49:54 +0530

    Time: 9 ms (50.99%) | Memory: 52.5 MB (96.95%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,48 +1,51 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
+        int count=0;
+      
         int n = grid[0].length;
-        Queue<int[]> q = new LinkedList<>();
-                 int count = 0;
-         boolean vis[][] = new boolean[m][n];
-         
+          boolean vis[][] = new boolean [m][n];
 
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if((i==0 || i==m-1 ||j==0 ||j==n-1) && grid[i][j] ==1){
-                    q.offer(new int[]{i,j});
-                    vis[i][j] = true;
+            for(int j=0;j<n;j++){
+                if((i==0 || i==m-1 || j==0 || j==n-1) && grid[i][j]==1){
+                    vis[i][j]=true;
+                    q.offer(new int[] {i,j});
                 }
             }
         }
 
-        int[] delrow = {-1,0,+1,0};
-        int[]delcol = {0,-1, 0, +1};
+     int[] delrow = {-1, 0 , +1, 0};
+     int[] delcol = { 0, -1, 0, +1};
 
-        while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int row =curr[0];
-            int col =curr[1];
-            
-            for(int i = 0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                  if(nrow>0 && nrow<m & ncol>=0 && ncol<n && vis[nrow][ncol]==false && grid[nrow][ncol]==1){
-                 vis[nrow][ncol]=true;
-                 q.offer(new int[]{nrow, ncol});
+     while(!q.isEmpty()){
+        int[] curr = q.poll();
+        int row = curr[0];
+        int col = curr[1];
+
+        for(int i=0;i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol  = col + delcol[i];
+
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==false && grid[nrow][ncol]==1){
+                vis[nrow][ncol]=true;
+                q.offer(new int[]{nrow, ncol});
             }
-            }
-          
+
 
         }
-       for(int i =0;i<m;i++){
+
+
+     }
+     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(grid[i][j]==1 && vis[i][j]== false){
+            if(vis[i][j]==false && grid[i][j] ==1){
                 count++;
             }
-        }
-       }
-
+     }
+     }
        return count;
     }
-}
+  
+    }
