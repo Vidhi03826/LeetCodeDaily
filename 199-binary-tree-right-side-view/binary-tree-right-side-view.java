e9946cf6a11ee61b67c9137c66a7e7edcbfbef34
Vidhi03826 <vidh312@gmail.com>
2026-06-16 01:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (92.01%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,19 +15,17 @@
  */
 class Solution {
     public void helper(TreeNode curr, List<Integer> res, int currdepth){
-       if(curr==null) return;
-       if(currdepth==res.size()){
-        res.add(curr.val);
-       }
-        helper(curr.right,res, currdepth+1);
-        helper(curr.left,res, currdepth+1);
-
+        if(curr ==null) return;
+        if(currdepth==res.size()){
+            res.add(curr.val);
+        }
+        helper(curr.right, res,currdepth+1);
+        helper(curr.left, res, currdepth+1);
+      
     }
     public List<Integer> rightSideView(TreeNode root) {
-         List<Integer> res = new ArrayList<Integer>();
-        
-          helper(root, res, 0);
-          return res;
-
+        List<Integer> res = new ArrayList<>();
+        helper(root, res ,0);
+        return res;
     }
 }
