2e8da75d7869bcf1321b9cf47ead34ce77cfb4e2
Vidhi03826 <vidh312@gmail.com>
2026-07-19 01:20:28 +0530

    Time: 2 ms (41.26%) | Memory: 45.1 MB (65.87%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -17,38 +17,25 @@
 
 class Solution {
    
-    public int widthOfBinaryTree(TreeNode root) {
+ public int widthOfBinaryTree(TreeNode root) {
+    if (root == null){
+         return 0;
+    }
+     int maxWidth = 0;
+     Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
+      q.offer(new Pair<>(root, 0));
 
-       
-        if (root == null)
-            return 0;
-
-        int maxWidth = 0;
-
-       
-        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
-
-    
-        q.offer(new Pair<>(root, 0));
-
-        
         while (!q.isEmpty()) {
-
-           
             int size = q.size();
             int minIndex = q.peek().getValue();
-
             int first = 0, last = 0;
 
-            
             for (int i = 0; i < size; i++) {
-
-              
-                Pair<TreeNode, Integer> p = q.poll();
+             Pair<TreeNode, Integer> p = q.poll();
+             
                 TreeNode node = p.getKey();
                 int currIndex = p.getValue() - minIndex;
-
-                if (i == 0)
+                 if (i == 0)
                     first = currIndex;
 
                 if (i == size - 1)
