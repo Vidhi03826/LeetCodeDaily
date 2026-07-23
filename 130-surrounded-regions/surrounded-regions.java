a4dec6cd8b5bbdef771261748eeaf326447b2e3c
Vidhi03826 <vidh312@gmail.com>
2026-07-24 01:58:48 +0530

    Time: 2 ms (73.12%) | Memory: 47.8 MB (57.60%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,49 +1,47 @@
 class Solution {
     public void solve(char[][] board) {
         int m = board.length;
-        int n = board[0].length;
-        int vis[][] = new int[m][n];
-        int [] delrow = {-1,0,+1,0};
-        int [] delcol = {0,+1,0,-1};
+        int n= board[0].length;
+        int[][] vis = new int[m][n];
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
 
-
-        for(int j=0;j<n;j++){
+        for(int j =0;j<n;j++){
             if(vis[0][j]==0 && board[0][j]=='O'){
-                dfs(0, j , delrow, delcol,board,vis, m , n);
+                dfs(0,j,delrow, delcol, vis, board, m ,n);
             }
-            if(vis[m-1][j] ==0 && board[m-1][j]=='O'){
-                dfs(m-1, j, delrow, delcol, board,vis, m , n);
+            if(vis[m-1][j]==0 && board[m-1][j]=='O'){
+                dfs(m-1,j,delrow, delcol, vis, board, m ,n);
             }
         }
-        for(int i=0;i<m;i++){
+        for(int i =0;i<m;i++){
             if(vis[i][0]==0 && board[i][0]=='O'){
-                dfs(i, 0 , delrow, delcol,board,vis, m , n);
+                dfs(i,0,delrow, delcol, vis, board, m ,n);
             }
             if(vis[i][n-1]==0 && board[i][n-1]=='O'){
-                dfs(i, n-1, delrow, delcol, board,vis, m , n);
+                dfs(i,n-1,delrow, delcol, vis, board, m ,n);
             }
         }
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(vis[i][j]==0 && board[i][j] == 'O') {
-                    board[i][j] = 'X';
+                if(vis[i][j]==0 && board[i][j]=='O'){
+                    board[i][j]='X';
                 }
             }
         }
-    }
 
-        private void dfs(int row, int col, int[] delrow, int[] delcol, char[][] board, int[][]vis, int m , int n){
-             
+        
+    }
+    public void dfs(int row, int col,int[] delrow, int[] delcol,  int[][] vis, char[][] board, int m, int n){
             vis[row][col]=1;
             for(int i=0;i<4;i++){
-             
-                    int nrow = row + delrow[i];
-                    int ncol = col + delcol[i];
-                    if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && board[nrow][ncol]=='O')
-                    dfs(nrow, ncol, delrow, delcol,board, vis, m,n);
-                }
-            
-        }
-    }
+                int nrow = row + delrow[i];
+                int ncol = col+ delcol[i];
 
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && board[nrow][ncol]=='O'){
+                    dfs(nrow, ncol, delrow, delcol, vis, board, m, n);
+                }
+            }
+        }
+}
