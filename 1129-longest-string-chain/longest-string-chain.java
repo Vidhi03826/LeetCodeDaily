5e928ce4865eaa60e9810c023d3d15a4ef9d66b9
Vidhi03826 <vidh312@gmail.com>
2026-08-02 02:07:07 +0530

    Time: 56 ms (32.62%) | Memory: 46.2 MB (75.44%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -2,41 +2,40 @@ class Solution {
     public int longestStrChain(String[] words) {
         int n = words.length;
 
-              Arrays.sort(words, Comparator.comparingInt(String::length));
-
+        Arrays.sort(words, Comparator.comparingInt(String::length));
 
         int[] dp = new int[n];
-            Arrays.fill(dp, 1);
-                    int maxlen = 0;
+        Arrays.fill(dp, 1);
+        int maxlen = 0;
 
-
-        for(int i=0;i<n;i++){
-            for(int j= 0;j<i ; j++){
-                if(checkpossible(words[i] , words[j])  && dp[j]+1>dp[i]){
-                    dp[i]= dp[j]+1;          
-                 }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (checkpossible(words[i], words[j]) && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
             }
-            if(maxlen<dp[i]){
+            if (maxlen < dp[i]) {
                 maxlen = dp[i];
             }
         }
         return maxlen;
     }
 
-    private boolean checkpossible(String s , String t){
-        if(s.length()!= t.length()+1){return false;}
-      int i = 0;
-      int j = 0;
-        while(i<s.length()){
-            if(j<t.length() && s.charAt(i)==t.charAt(j)){
+    private boolean checkpossible(String s, String t) {
+        if (s.length() != t.length() + 1) {
+            return false;
+        }
+        int i = 0;
+        int j = 0;
+        while (i < s.length()) {
+            if (j < t.length() && s.charAt(i) == t.charAt(j)) {
                 i++;
                 j++;
 
-            }
-            else{
+            } else {
                 i++;
             }
         }
-        return(i==s.length() && j==t.length());
+        return (i == s.length() && j == t.length());
     }
 }
