6bbacf7efa77f0b18231ba463807d550926c7683
Vidhi03826 <vidh312@gmail.com>
2026-09-07 02:23:48 +0530

    Time: 21 ms (33.76%) | Memory: 54.2 MB (63.00%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int helper(int i, int j, String s, String t, int [][] dp ){
-      int m = s.length();
-        int n = t.length();
-        
+    public int helper(int i , int j ,String s, String t , int[][] dp ){
         if(j==0) return 1;
         if(i==0) return 0;
-       
-        if(dp[i][j] !=-1) return dp[i][j];
-       
+
+        if(dp[i][j]!= -1){
+            return dp[i][j];
+        }
+
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            int take = helper(i-1  , j-1 , s , t , dp);
+            int nottake = helper(i-1 , j , s, t ,dp);
+
+           return dp[i][j] = take + nottake;
+        }
+        else{
+           return dp[i][j] = helper(i-1 , j , s , t, dp);
+        }
     
-        if(s.charAt(i-1)== t.charAt(j-1)) {
-            int take =  helper( i-1,  j-1,  s,  t, dp);
-            int notTake = helper( i-1,  j,  s,  t, dp);
-            return dp[i][j] =  take + notTake;
-        }
-        else{// if string notmtched
-           return dp[i][j]  = helper( i-1,  j,  s,  t, dp);
-        }
-       
     }
     public int numDistinct(String s, String t) {
-       int[][] dp = new int[s.length()+1][t.length()+1];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        for(int[] row : dp){
+            Arrays.fill(row , -1);
+        }
 
-        // Start recursion
-        return helper(s.length(), t.length(), s, t, dp);
+        return helper(s.length() , t.length() , s , t , dp);
     }
 }
-
