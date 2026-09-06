704580e8f27c2924840f1eba3ff0ea6991f543b8
Vidhi03826 <vidh312@gmail.com>
2026-09-06 23:35:17 +0530

    Time: 15 ms (43.25%) | Memory: 49.4 MB (28.90%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,40 +2,38 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        Queue<int [] > q = new LinkedList<>();
         int[][] dist = new int[m][n];
-        boolean [][] vis = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[m][n];
 
-        //push all zeroes in the q
-        for(int i =0;i<m;i++){
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j] ==0){
-                   q.offer(new int[]{i,j,0});
-                   vis[i][j] = true;
+                if(mat[i][j]==0){
+                    q.add(new int[]{i,j,0});
+                    vis[i][j] = true;
                 }
             }
         }
-        int[] delrow = {-1,0,+1,0};
-        int [] delcol = {0,1,0,-1};
 
         while(!q.isEmpty()){
-            int[] curr = q.poll();
+            int[] curr = q.remove();
             int row = curr[0];
             int col = curr[1];
             int steps = curr[2];
-            dist[row][col] =steps;
+
+            dist[row][col] = steps;
 
             for(int i=0;i<4;i++){
-             
-                    int nrow = row + delrow[i];
-                    int ncol = col + delcol[i];
-                    
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
 
-                    if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==false){
-                        vis[nrow][ncol]=true;
-                        q.offer(new int[]{nrow, ncol, steps+1});
-                    }
-                
+                if(nrow>=0 && nrow<m & ncol>=0 && ncol<n && !vis[nrow][ncol]){
+                    q.add(new int[]{nrow , ncol , steps+1});
+                    vis[nrow][ncol] = true;
+                }
             }
         }
         return dist;
